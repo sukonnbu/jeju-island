a5a79f1c566ef8b50912e4b54992b9725557cb11
sukonnbu <choijiwan1206@gmail.com>
2024-05-09 00:22:42 +0900

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <div className="navbar__logo">
+          <a href="">KH JEJU MAP</a>
+        </div>
+        <ul className="navbar__menu">
+          <li><a href="">Home</a></li>
+          <li><a href="">Route</a></li>
+          <li><a href="">Place</a></li>
+          <li><a href="">Item</a></li>
+        </ul>
+      </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
