c8152f9e0b53d1ae21145348a4d9688a49791dff
sukonnbu <choijiwan1206@gmail.com>
2024-05-20 01:07:39 +0900

    merge with sseungyong

--- src/routes/Schedule.js
+++ src/routes/Schedule.js
@@ -18,13 +18,13 @@ function Schedule () {
         </div>
         <div className="flex flex-col items-center">
             {team === "1" && <article>A조
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAyNDA1MjBfMTgg/MDAxNzE2MTMzNDIwNTE5.jcA0GmLGK10LEeUa6OejaGH8aPKrhGnIhrHlVX0I88gg.NZ4gtqFtVnIPPygAcgTxOmoducpp5aROzAiwh1Be2d4g.JPEG/%EC%9D%BC%EC%A0%95_A.jpg?type=w800" alt="A조 일정"/>      
+            <img src="../images/schedule/일정_A.jpg" alt="A조 일정"/>      
             </article>}
             {team === "2" && <article>B조
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAyNDA1MjBfMTU4/MDAxNzE2MTMzNzg4NzM1.yrIZdVFuddYk7EUiZWB22yW3gAYoY0m3mrO42XZbd8sg.E_kIfu9TekzHZ6vBPLQKfTPjDcVuXvSyv4VBVq9yBNUg.JPEG/%EC%9D%BC%EC%A0%95_B.jpg?type=w800" alt="B조 일정"/>      
+            <img src="../images/schedule/일정_B.jpg" alt="B조 일정"/>      
             </article>}
             {team === "3" && <article>C조
-            <img src="https://mblogthumb-phinf.pstatic.net/MjAyNDA1MjBfMjc5/MDAxNzE2MTM0MDYxMDYw.ijcsmjGzjk0Wb-1leC-0AMkzGWTmdzHaxFnJ2qHuGvgg.Wd3AfvEZkD-MdswtQTqVttIY9zK0-dSTvy-aXC6VZpIg.JPEG/%EC%9D%BC%EC%A0%95_C.jpg?type=w800" alt="C조 일정"/>    
+            <img src="../images/schedule/일정_C.jpg" alt="C조 일정"/>    
             </article>}
         </div>
     </div>
